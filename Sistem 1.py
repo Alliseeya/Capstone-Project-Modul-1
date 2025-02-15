75fa10a06bb2e1dc675967e5030d5619139a0dfa
Alisya Ramadhani F <alisyaramadhanifatin@gmail.com>
2025-02-15 13:40:21 +0700

    Validasi input

--- Sistem 1.py
+++ Sistem 1.py
@@ -9,3 +9,63 @@ from tabulate import tabulate
 
 def read_buku():
     print(tabulate(buku, headers='keys', tablefmt='fancy_grid'))
+
+# Login pengguna
+user = [{'username':'TINAL', 'password':'Alliseeya5', 'role':'admin'},
+        {'username':'RANAL', 'password':'Samudra02', 'role':'pengunjung'}]
+
+def login():
+    username = input('Masukkan username: ')
+    password = input('Masukkan password: ')
+    for u in user:
+        if username == u['username'] and password == u['password']:
+            print('Login berhasil')
+            if u['role'] == 'admin':
+                return main()
+            elif u['role'] == 'pengunjung':
+                return pengunjung()
+    print('Login gagal')
+    return login()
+
+# Validasi inputan
+import re
+
+def generate_book_id(judul, tahun):
+    while True:
+        singkatan = ''.join([word[:2].upper() for word in judul.split()])
+        tahun_akhir = str(tahun)[-2:]
+        return singkatan + tahun_akhir
+
+def validasi_judul(prompt):
+    while True:
+        inputan = input(prompt)
+        if re.fullmatch(r'\w+\s*\w+', inputan):
+            return inputan
+        else:
+            print("Input tidak valid. Silakan coba lagi.")
+
+def validasi_pengarang(prompt):
+    while True:
+        inputan = input(prompt)
+        if re.fullmatch(r'\w+\s*\w+', inputan):
+            return inputan
+        else:
+            print("Input tidak valid. Silakan coba lagi.")
+
+def validasi_tahun_terbit(prompt):
+    while True:
+        inputan = input(prompt)
+        if re.fullmatch(r'\d{4}', inputan):
+            return inputan
+        else:
+            print("Input tidak valid. Silakan coba lagi.")
+
+def validasi_status(prompt):
+    while True:
+        inputan = input(prompt).title()
+        if re.fullmatch(r'Dikembalikan', inputan):
+            return inputan
+        elif re.fullmatch(r'Dipinjamkan', inputan):
+            return inputan
+        else:
+            print("Input tidak valid. Silakan coba lagi.")
